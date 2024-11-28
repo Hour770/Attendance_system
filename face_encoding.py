12a671a9ddddeecc55bb260a173489c33694e05b
Hour770 <88095949+Hour770@users.noreply.github.com>
2024-11-28 13:25:22 +0700

    face_encoding.py

--- face_encoding.py
+++ face_encoding.py
@@ -9,6 +9,7 @@ def load_encodings():
         known_face_names (list): A list of names corresponding to each encoding.
         known_face_genders (dict): A dictionary mapping names to gender.
     """
+    #replace with user image from your device. up to 10-20 images give more accuracy.
     hour_images = [
         "C:/Users/U-ser/OneDrive - American University of Phnom Penh/Desktop/image/hour.png",
         "C:/Users/U-ser/OneDrive - American University of Phnom Penh/Desktop/image/hour1.png",
@@ -30,7 +31,7 @@ def load_encodings():
         "C:/Users/U-ser/OneDrive - American University of Phnom Penh/Desktop/image/hour17.png",
         "C:/Users/U-ser/OneDrive - American University of Phnom Penh/Desktop/image/hour18.png",
         "C:/Users/U-ser/OneDrive - American University of Phnom Penh/Desktop/image/hour19.png",
-    ] #replace with the user image from your device
+    ]
     hour_encodings = []
     for image_path in hour_images:
         image = face_recognition.load_image_file(image_path)
@@ -38,17 +39,48 @@ def load_encodings():
         if encodings:
             hour_encodings.append(encodings[0])
 
+    messi_images = [
+        "C:/Users/U-ser/OneDrive - American University of Phnom Penh/Desktop/image/messi1.jpg",
+        "C:/Users/U-ser/OneDrive - American University of Phnom Penh/Desktop/image/messi2.webp",
+        "C:/Users/U-ser/OneDrive - American University of Phnom Penh/Desktop/image/messi3.jpg",
+        "C:/Users/U-ser/OneDrive - American University of Phnom Penh/Desktop/image/messi4.jpg",
+        "C:/Users/U-ser/OneDrive - American University of Phnom Penh/Desktop/image/messi5.jpg",
+        "C:/Users/U-ser/OneDrive - American University of Phnom Penh/Desktop/image/messi6.jpg",
+        "C:/Users/U-ser/OneDrive - American University of Phnom Penh/Desktop/image/messi7.jpg",
+        "C:/Users/U-ser/OneDrive - American University of Phnom Penh/Desktop/image/messi8.webp",
+        "C:/Users/U-ser/OneDrive - American University of Phnom Penh/Desktop/image/messi9.webp",
+        "C:/Users/U-ser/OneDrive - American University of Phnom Penh/Desktop/image/messi10.jpg",
+        "C:/Users/U-ser/OneDrive - American University of Phnom Penh/Desktop/image/messi11.webp",
+        "C:/Users/U-ser/OneDrive - American University of Phnom Penh/Desktop/image/messi12.jpg",
+        "C:/Users/U-ser/OneDrive - American University of Phnom Penh/Desktop/image/messi13.webp",
+        "C:/Users/U-ser/OneDrive - American University of Phnom Penh/Desktop/image/messi14.jpg",
+        "C:/Users/U-ser/OneDrive - American University of Phnom Penh/Desktop/image/messi15.jpg",
+        "C:/Users/U-ser/OneDrive - American University of Phnom Penh/Desktop/image/messi16.jpg",
+        "C:/Users/U-ser/OneDrive - American University of Phnom Penh/Desktop/image/messi17.jpg",
+        "C:/Users/U-ser/OneDrive - American University of Phnom Penh/Desktop/image/messi18.jpg",
+        "C:/Users/U-ser/OneDrive - American University of Phnom Penh/Desktop/image/messi19.jpg",
+        "C:/Users/U-ser/OneDrive - American University of Phnom Penh/Desktop/image/messi20.jpg",
+    ]
+    messi_encodings = []
+    for image_path in messi_images:
+        image = face_recognition.load_image_file(image_path)
+        encodings = face_recognition.face_encodings(image)
+        if encodings:
+            messi_encodings.append(encodings[0])
 
     # Combine all encodings and names
     known_face_encodings = (
-        hour_encodings
+        hour_encodings +
+        messi_encodings
     )
     known_face_names = (
-        ["Hour"] * len(hour_encodings)
+        ["Hour"] * len(hour_encodings)+
+        ['Messi'] * len(messi_encodings)
     )
 
     known_face_genders = {
-        'Hour': 'Male'
+        'Hour': 'Male',
+        'Messi' : 'Male'
     }
 
     return known_face_encodings, known_face_names, known_face_genders
